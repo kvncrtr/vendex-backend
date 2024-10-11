cc31f4fc11375e5226a15204bb34e55c4c432b74
Kevin Carter <console.logkevin@gmail.com>
2024-10-11 16:41:55 -0400

    add init .env

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/kvncrtr/vendex/db"
 	"github.com/kvncrtr/vendex/routes"
 	_ "github.com/lib/pq"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 	// Init Database
 	db.InitDB()
 	defer db.DB.Close()
